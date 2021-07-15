81820e3452873894e7ce85fa28c211600fe194c1
Luke Hartman <ljhartman10@gmail.com>
2021-07-15 16:48:31 -0700

    Add default argument for index on utilities::getUnit

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -25,7 +25,11 @@ export const getAngle = (x: number, y: number) => {
   return (Math.atan2(y, x) * 180) / Math.PI;
 };
 
-export const getUnit = (number: number, range: number, index: number) => {
+export const getUnit = (
+  number: number,
+  range: number,
+  index: number | undefined = undefined
+) => {
   let value = number % range;
 
   if (index && getDigit(number, index) % 2 === 0) {
